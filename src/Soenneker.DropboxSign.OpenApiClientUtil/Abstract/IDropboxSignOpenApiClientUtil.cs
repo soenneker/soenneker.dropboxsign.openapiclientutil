519ddbb5ef9c33bc2082a3af8609f8ae34116129
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:09 -0500

    xml commenting

--- src/Soenneker.DropboxSign.OpenApiClientUtil/Abstract/IDropboxSignOpenApiClientUtil.cs
+++ src/Soenneker.DropboxSign.OpenApiClientUtil/Abstract/IDropboxSignOpenApiClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.DropboxSign.OpenApiClientUtil.Abstract;
 /// </summary>
 public interface IDropboxSignOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<DropboxSignOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
